0898ab2012359de57d4b16c44bc2d47c4bde0186
McGlock <mclaughlinr2@gmail.com>
2021-07-29 14:17:51 -0700

    edited to run full feature table

--- dev_utils/abund_plot_PR.py
+++ dev_utils/abund_plot_PR.py
@@ -15,7 +15,7 @@ sns.set_style('whitegrid')
 sns.set(font_scale=1.0)
 
 # TODO: build plot to investigate best hyperparams based on starting completeness
-pred_file = '/home/ryan/Desktop/test_NMF/CAMI_high_GoldStandardAssembly.nmf_scores.tsv'
+pred_file = '/home/ryan/Desktop/test_NMF/CAMI_high_GoldStandardAssembly.all_scores.tsv'
 src2sag_file = '/home/ryan/Desktop/test_NMF/src2sag_map.tsv'
 abund_file = '/home/ryan/Desktop/test_NMF/CAMI_high_GoldStandardAssembly.nmf_trans_20.tsv'
 
@@ -223,7 +223,7 @@ pred_stack_df = pred_stack_df.sort_values(['nu', 'gamma_sorter'], ascending=[Tru
 ax = sns.catplot(x="metric", y="score", hue="gamma", kind='box',
                  data=pred_stack_df, aspect=2, palette=sns.light_palette("black"),
                  flierprops=flierprops)
-plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots/gamma_boxplot.png',
+plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots_all/gamma_boxplot.png',
             bbox_inches='tight', dpi=300)
 plt.clf()
 plt.close()
@@ -231,7 +231,7 @@ plt.close()
 ax = sns.catplot(x="metric", y="score", hue="nu", kind='box',
                  data=pred_stack_df, aspect=2, palette=sns.light_palette("black"),
                  flierprops=flierprops)
-plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots/nu_boxplot.png',
+plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots_all/nu_boxplot.png',
             bbox_inches='tight', dpi=300)
 plt.clf()
 plt.close()
@@ -250,9 +250,10 @@ for level in lev_dict:
         print(count_df.shape)
         count_df.columns = ['level', 'inclusion', 'nu', 'gamma', 'count']
         count_df = count_df.sort_values(['count'], ascending=[False])
-        count_df.to_csv('/home/ryan/Desktop/test_NMF/PR_plots/' + level + '_' + inclusion + '_top_ranked_params.tsv',
-                        index=False, sep='\t'
-                        )
+        count_df.to_csv(
+            '/home/ryan/Desktop/test_NMF/PR_plots_all/' + level + '_' + inclusion + '_top_ranked_params.tsv',
+            index=False, sep='\t'
+            )
         top_level = count_df['level'].iloc[0]
         top_inclusion = count_df['inclusion'].iloc[0]
         top_nu = count_df['nu'].iloc[0]
@@ -263,7 +264,7 @@ for level in lev_dict:
         nu_order = list(sorted(set(count_df['nu'])))
         g = sns.FacetGrid(count_df, col="gamma", col_wrap=4)
         g.map(sns.barplot, "nu", "count", order=nu_order, ci=None)
-        plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots/' + level + '_' + inclusion + '_nu_gamma_facetplot.png')
+        plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots_all/' + level + '_' + inclusion + '_nu_gamma_facetplot.png')
         plt.clf()
         plt.close()
 
@@ -281,11 +282,11 @@ grp_metrics_df = top_config_df[['level', 'inclusion', 'nu_gamma', 'nu', 'gamma',
                                                  MCC_mean=('MCC', 'mean'),
                                                  MCC_cnt=('MCC', 'count')).reset_index()
 
-grp_metrics_df.to_csv('/home/ryan/Desktop/test_NMF/PR_plots/top_config_stats.tsv',
+grp_metrics_df.to_csv('/home/ryan/Desktop/test_NMF/PR_plots_all/top_config_stats.tsv',
                       sep='\t', index=False)
 
 best_metrics_df = grp_metrics_df.loc[grp_metrics_df['MCC_cnt'] == grp_metrics_df['MCC_cnt'].max()]
-best_metrics_df.to_csv('/home/ryan/Desktop/test_NMF/PR_plots/best_config_stats.tsv',
+best_metrics_df.to_csv('/home/ryan/Desktop/test_NMF/PR_plots_all/best_config_stats.tsv',
                        sep='\t', index=False)
 best_nu = best_metrics_df['nu'].iloc[0]
 best_gamma = best_metrics_df['gamma'].iloc[0]
@@ -309,7 +310,7 @@ text_str = ''.join(['Mean:\n  P=', str(round(mean_P, 2)), '\n  R=', str(round(me
                     ])
 for ax in g.axes.flat:
     ax.text(7, 200, text_str, fontsize=9)
-plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots/best_config_histo_precision.png',
+plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots_all/best_config_histo_precision.png',
             bbox_inches='tight', dpi=300)
 plt.clf()
 plt.close()
@@ -320,7 +321,7 @@ text_str = ''.join(['Mean:\n  P=', str(round(mean_P, 2)), '\n  R=', str(round(me
                     ])
 for ax in g.axes.flat:
     ax.text(7, 100, text_str, fontsize=9)
-plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots/best_config_histo_sensitivity.png',
+plt.savefig('/home/ryan/Desktop/test_NMF/PR_plots_all/best_config_histo_sensitivity.png',
             bbox_inches='tight', dpi=300)
 plt.clf()
 plt.close()

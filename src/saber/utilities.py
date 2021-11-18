c29f0ce4e17d0fbf7f49d7b2f4c3d744032b34c4
McGlock <mclaughlinr2@gmail.com>
2021-11-18 13:56:24 -0800

    testing new autoopt param method

--- src/saber/utilities.py
+++ src/saber/utilities.py
@@ -380,9 +380,7 @@ def set_clust_params(denovo_min_clust, denovo_min_samp, anchor_min_clust,
     for i, p in enumerate(params_tmp):  # if user wants one custom param, but maybe this should be allow?
         if p is not None:
             params_list[i] = float(p)
-    print(a)
-    print(params_list)
-    sys.exit()
+
     logging.info('AutoOpt method:', str(a))
     logging.info('\tDe Novo min_cluster_size: ' + str(params_list[0]))
     logging.info('\t        min_samples: ' + str(params_list[1]))

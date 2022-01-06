1e36313168de90de8ef0c61779765cd7f28ea313
Rylobyte <mclaughlinr2@gmail.com>
2022-01-06 15:31:09 -0800

    running both

--- dev_utils/run_benchmarking_errstat.py
+++ dev_utils/run_benchmarking_errstat.py
@@ -5,6 +5,7 @@ sys.path.append('/home/ryan/dev/SABer')
 sys.path.append('/home/ryan/dev/SABer/src')
 sys.path.append('/home/ryan/dev/SABer/src/saber')
 import vamb_errstat
+import saber_errstat
 
 # Input files directory
 working_dir = sys.argv[1]
@@ -14,12 +15,11 @@ sample = sys.argv[4]
 threads = int(sys.argv[5])
 
 # SABer errstat
-'''
+
 mockpath = os.path.join(synthdata_dir, 'Final_SAGs_20k_test_subset/' + str(sample) + '/')
 run_err_df = saber_errstat.runErrorAnalysis(working_dir, synthdata_dir, mg_asm, mockpath, threads)
 run_err_df.to_csv(os.path.join(working_dir, 'Bin.errstat.tsv'), sep='\t', index=False)
 print(run_err_df)
-'''
 
 # VAMB errstat
 run_err_df = vamb_errstat.runErrorAnalysis(working_dir, synthdata_dir, mg_asm, threads)

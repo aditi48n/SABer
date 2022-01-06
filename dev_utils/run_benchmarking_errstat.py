e8e66911f6721f2cc61a6b0a32f1079eef25ab2c
Rylobyte <mclaughlinr2@gmail.com>
2022-01-06 15:38:34 -0800

    flag for binner

--- dev_utils/run_benchmarking_errstat.py
+++ dev_utils/run_benchmarking_errstat.py
@@ -12,16 +12,20 @@ working_dir = sys.argv[1]
 synthdata_dir = sys.argv[2]
 mg_asm = sys.argv[3]
 sample = sys.argv[4]
-threads = int(sys.argv[5])
+binner = sys.argv[5]
+threads = int(sys.argv[6])
 
 # SABer errstat
+if binner == 'SABer':
+    mockpath = os.path.join(synthdata_dir, 'Final_SAGs_20k_test_subset/' + str(sample) + '/')
+    run_err_df = saber_errstat.runErrorAnalysis(working_dir, synthdata_dir, mg_asm, mockpath, threads)
+    run_err_df.to_csv(os.path.join(working_dir, 'Bin.errstat.tsv'), sep='\t', index=False)
+    print(binner)
+    print(run_err_df)
 
-mockpath = os.path.join(synthdata_dir, 'Final_SAGs_20k_test_subset/' + str(sample) + '/')
-run_err_df = saber_errstat.runErrorAnalysis(working_dir, synthdata_dir, mg_asm, mockpath, threads)
-run_err_df.to_csv(os.path.join(working_dir, 'Bin.errstat.tsv'), sep='\t', index=False)
-print(run_err_df)
-
-# VAMB errstat
-run_err_df = vamb_errstat.runErrorAnalysis(working_dir, synthdata_dir, mg_asm, threads)
-run_err_df.to_csv(os.path.join(working_dir, 'Bin.errstat.tsv'), sep='\t', index=False)
-print(run_err_df)
+if binner == 'VAMB':
+    # VAMB errstat
+    run_err_df = vamb_errstat.runErrorAnalysis(working_dir, synthdata_dir, mg_asm, threads)
+    run_err_df.to_csv(os.path.join(working_dir, 'Bin.errstat.tsv'), sep='\t', index=False)
+    print(binner)
+    print(run_err_df)

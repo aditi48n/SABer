a556798279b58e71825e7bc249b030fc992df6f7
Rylobyte <mclaughlinr2@gmail.com>
2022-01-10 14:32:01 -0800

    processing error stat for other binners

--- dev_utils/run_benchmarking_errstat.py
+++ dev_utils/run_benchmarking_errstat.py
@@ -6,6 +6,7 @@ sys.path.append('/home/ryan/dev/SABer/src')
 sys.path.append('/home/ryan/dev/SABer/src/saber')
 import vamb_errstat
 import saber_errstat
+import unitem_errstat
 
 # Input files directory
 working_dir = sys.argv[1]
@@ -26,6 +27,13 @@ if binner == 'SABer':
 if binner == 'VAMB':
     # VAMB errstat
     run_err_df = vamb_errstat.runErrorAnalysis(working_dir, synthdata_dir, mg_asm, threads)
+    run_err_df.to_csv(os.path.join(working_dir, 'Bin.errstat.tsv'), sep='\t', index=False)
+    print(binner)
+    print(run_err_df)
+
+if binner == 'UNITEM':
+    # UNITEM errstat
+    run_err_df = unitem_errstat.runErrorAnalysis(working_dir, synthdata_dir, mg_asm, threads)
     run_err_df.to_csv(os.path.join(working_dir, 'Bin.errstat.tsv'), sep='\t', index=False)
     print(binner)
     print(run_err_df)

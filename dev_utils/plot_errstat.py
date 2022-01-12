65bb0d8e348d27e81abc3e45c16eaecf570e8bb9
Rylobyte <mclaughlinr2@gmail.com>
2022-01-12 11:48:41 -0800

    building boxplots

--- dev_utils/plot_errstat.py
+++ dev_utils/plot_errstat.py
@@ -77,7 +77,7 @@ sm_abs_str_df.sort_values(by=['type_rank', 'algo_rank',
 
 # Boxplots for mode and param set
 ss_box = sns.catplot(x="mode", y="ext_nc_uniq", hue="algo",
-                     col="param_set", col_wrap=4,
+                     col="param_set", col_wrap=2,
                      kind="box", data=ss_abs_str_df, notch=True,
                      linewidth=0.75, saturation=0.75, width=0.75,
                      palette=sns.color_palette("muted")
@@ -88,7 +88,7 @@ ss_box.savefig(os.path.join(workdir, 'SABer.single.mode_param.boxplot.png'),
 plt.clf()
 plt.close()
 sm_box = sns.catplot(x="mode", y="ext_nc_uniq", hue="algo",
-                     col="param_set", col_wrap=4,
+                     col="param_set", col_wrap=2,
                      kind="box", data=sm_abs_str_df, notch=True,
                      linewidth=0.75, saturation=0.75, width=0.75,
                      palette=sns.color_palette("muted")

d7bf2e2c50b58da35b9eea6e469337f5844951aa
Rylobyte <mclaughlinr2@gmail.com>
2022-01-12 10:20:23 -0800

    building boxplots

--- dev_utils/plot_errstat.py
+++ dev_utils/plot_errstat.py
@@ -1,6 +1,9 @@
+import os
 import sys
 
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 saber_single_file = sys.argv[1]
 saber_multi_file = sys.argv[2]
@@ -8,9 +11,21 @@ unitem_single_file = sys.argv[3]
 unitem_multi_file = sys.argv[4]
 # vamb_multi_file = sys.argv[5] # when it's ready
 
+# working directory
+workdir = os.path.dirname(saber_single_file)
+
 # Munge SABer single/mulit
 ss_df = pd.read_csv(saber_single_file, header=0, sep='\t')
+ss_abs_str_df = ss_df.query("level == strain_absolute")
 sm_df = pd.read_csv(saber_multi_file, header=0, sep='\t')
+sm_abs_str_df = sm_df.query("level == strain_absolute")
+
+# Build boxplots
+ss_box = sns.catplot(x="sample_type", y="ext_nc_uniq", hue="algo", kind="box", data=ss_abs_str_df)
+sm_box = sns.catplot(x="sample_type", y="ext_nc_uniq", hue="algo", kind="box", data=sm_abs_str_df)
+plt.savefig(os.path.join(workdir, 'SABer.single.boxplot.png'), dpi=300)
+plt.clf()
+plt.close()
 
 print(ss_df.head())
 print(sm_df.head())

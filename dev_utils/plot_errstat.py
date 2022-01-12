d5e916d110a2ca9a8fefe67b43272e4fe8899f48
Rylobyte <mclaughlinr2@gmail.com>
2022-01-12 10:34:41 -0800

    building boxplots

--- dev_utils/plot_errstat.py
+++ dev_utils/plot_errstat.py
@@ -17,21 +17,31 @@ unitem_multi_file = sys.argv[4]
 # working directory
 workdir = os.path.dirname(saber_single_file)
 
-# column renaming dictionaries
+# column renaming/mapping dictionaries
 type2label = {'CAMI_II_Airways': 'Air',
               'CAMI_II_Gastrointestinal': 'GI',
               'CAMI_II_Oral': 'Oral',
               'CAMI_II_Skin': 'Skin',
               'CAMI_II_Urogenital': 'Urog'
               }
+algo2rank = {'denovo': 0, 'hdbscan': 1,
+             'ocsvm': 2, 'intersection': 3
+             }
 
 # Munge SABer single/mulit
+# Near Complete
 ss_df = pd.read_csv(saber_single_file, header=0, sep='\t')
 ss_df['label'] = [type2label[x] for x in ss_df['sample_type']]
+ss_df['algo_rank'] = [algo2rank[x] for x in ss_df['algo']]
 ss_abs_str_df = ss_df.query("level == 'strain_absolute'")
+ss_abs_str_df.sort_values(by='algo_rank', inplace=True)
+
+# Medium Quality
 sm_df = pd.read_csv(saber_multi_file, header=0, sep='\t')
 sm_df['label'] = [type2label[x] for x in sm_df['sample_type']]
+sm_df['algo_rank'] = [algo2rank[x] for x in sm_df['algo']]
 sm_abs_str_df = sm_df.query("level == 'strain_absolute'")
+sm_abs_str_df.sort_values(by='algo_rank', inplace=True)
 
 # Build boxplots
 ss_box = sns.catplot(x="label", y="ext_nc_uniq", hue="algo", kind="box", data=ss_abs_str_df)

43453492b45befff59ed478f0a47ba1b5b19b533
Rylobyte <mclaughlinr2@gmail.com>
2022-01-12 16:15:23 -0800

    building boxplots

--- dev_utils/plot_errstat.py
+++ dev_utils/plot_errstat.py
@@ -60,10 +60,9 @@ ss_df['mode_paramset'] = [str(x) + '_' + str(y) for x, y in
 ss_df['algo_param'] = ['_'.join([str(x), str(y)]) for x, y in
                        zip(ss_df['algo'], ss_df['param_set'])
                        ]
-ss_df['label_sample_param'] = ['_'.join([str(x), str(y), str(z)]) for x, y, z in
-                               zip(ss_df['label'], ss_df['sample_id'],
-                                   ss_df['param_set'])
-                               ]
+ss_df['label_sample'] = ['_'.join([str(x), str(y), str(z)]) for x, y, z in
+                         zip(ss_df['label'], ss_df['sample_id'])
+                         ]
 ss_abs_str_df = ss_df.query("level == 'strain_absolute'")
 ss_abs_str_df.sort_values(by=['type_rank', 'algo_rank',
                               'mode_rank', 'param_set'

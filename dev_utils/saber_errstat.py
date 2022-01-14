3a0966719c2cfac3e32a0da119c87ab8ba875aef
Rylobyte <mclaughlinr2@gmail.com>
2022-01-14 10:43:06 -0800

    adding dnadiff and recall calc for xPGs

--- dev_utils/saber_errstat.py
+++ dev_utils/saber_errstat.py
@@ -216,10 +216,9 @@ def parse_report(p):
     data = pd.read_csv(report_file, skiprows=10, nrows=3, header=None)
     fix_list = []
     for i, row in data.iterrows():
-        print(row)
         r_list = [x for x in row[0].split(' ') if x != '']
         r_clean = [x.split('(', 1)[0] if '(' in x else x for x in r_list]
-        fix_list.append(r_list)
+        fix_list.append(r_clean)
     fixed_df = pd.DataFrame(fix_list,
                             columns=['stat', 'reference(bp)',
                                      'query(bp)']

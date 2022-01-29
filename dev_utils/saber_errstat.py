3971ae1a986fea55ffc46554d9c898fcf782c0b9
Rylobyte <mclaughlinr2@gmail.com>
2022-01-28 16:36:22 -0800

    fixing abs vs asm

--- dev_utils/saber_errstat.py
+++ dev_utils/saber_errstat.py
@@ -75,12 +75,13 @@ def xpg_stats(p):
     FP = calc_fp(pred_df['truth_strain'], pred_df['pred'], pred_df['contig_bp'])
     TN = calc_tn(pred_df['truth'], pred_df['pred'], pred_df['contig_bp'])
     FN = calc_fn(pred_df['truth'], pred_df['pred'], pred_df['contig_bp'])
-    abs_FN = calc_fn(pred_df['truth'], pred_df['pred'], pred_df['contig_bp']) - diff_TP
+    if FN != 0:
+        FN = calc_fn(pred_df['truth'], pred_df['pred'], pred_df['contig_bp']) - diff_TP
     # compute total possible bp for each genome
-    str_tot_bp_poss = TP + FN
+    str_tot_bp_poss = inter_TP + FN
     # Complete SRC genome is not always present in contigs, need to correct for that.
-    working_bp = tot_bp - TP - FN
-    corrected_FN = abs_FN + working_bp
+    working_bp = tot_bp - inter_TP - FN
+    corrected_FN = FN + working_bp
     str_list = calc_stats(sag_id, 'strain_assembly', algo, TP, FP, TN, FN,
                           pred_df['truth_strain'], pred_df['pred']
                           )
@@ -95,12 +96,13 @@ def xpg_stats(p):
     FP = calc_fp(pred_df['truth'], pred_df['pred'], pred_df['contig_bp'])
     TN = calc_tn(pred_df['truth'], pred_df['pred'], pred_df['contig_bp'])
     FN = calc_fn(pred_df['truth'], pred_df['pred'], pred_df['contig_bp'])
-    abs_FN = calc_fn(pred_df['truth'], pred_df['pred'], pred_df['contig_bp']) - diff_TP
+    if FN != 0:
+        FN = calc_fn(pred_df['truth'], pred_df['pred'], pred_df['contig_bp']) - diff_TP
     # compute total possible bp for each genome
-    exa_tot_bp_poss = TP + FN
+    exa_tot_bp_poss = inter_TP + FN
     # Complete SRC genome is not always present in contigs, need to correct for that.
-    working_bp = tot_bp - TP - FN
-    corrected_FN = abs_FN + working_bp
+    working_bp = tot_bp - inter_TP - FN
+    corrected_FN = FN + working_bp
     x_list = calc_stats(sag_id, 'exact_assembly', algo, TP, FP, TN, FN,
                         pred_df['truth'], pred_df['pred']
                         )

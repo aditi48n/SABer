d0a9df0845247536cd5325429f1ef274add351db
Rylobyte <mclaughlinr2@gmail.com>
2022-02-02 09:23:33 -0800

    checking where lag is happening

--- src/saber/__main__.py
+++ src/saber/__main__.py
@@ -21,35 +21,27 @@ Use '-h' to get subcommand-specific help, e.g.
 
 
 def main():
-    print(1)
     commands = {"recruit": recruit,
                 "info": info}
-    print(2)
     parser = argparse.ArgumentParser(description='Recruit environmental reads to reference SAG(s).',
                                      add_help=False
                                      )
-    print(3)
     parser.add_argument('command', nargs='?')
-    print(4)
     input_cmd = sys.argv[1:2]
     if input_cmd == ['-h']:
         input_cmd = ['help']
-    print(5)
     args = parser.parse_args(input_cmd)
-    print(6)
+
     if (not args.command) | (args.command == 'help'):
         sys.stderr.write(usage)
         sys.exit(1)
+
     elif args.command not in commands:
         logging.error('Unrecognized command')
         sys.stderr.write(usage)
         sys.exit(1)
-    print(7)
 
     cmd = commands.get(args.command)
-    print(8)
-    print(sys.argv[2:])
-    print(args.command)
     cmd(sys.argv[2:])
     logging.info("SABer has finished successfully.\n")
 

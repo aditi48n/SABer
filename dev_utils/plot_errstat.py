074de219d5b33cd818d5f672cb94c3d0818fd20a
Rylobyte <mclaughlinr2@gmail.com>
2022-02-02 11:41:50 -0800

    no de novo

--- dev_utils/plot_errstat.py
+++ dev_utils/plot_errstat.py
@@ -382,7 +382,7 @@ cat_cnt_df['binner'] = [x.split('_', 2)[0] + '_' + x.split('_', 2)[1]
                         if 'SABer' in x else x.split('_', 1)[0]
                         for x in cat_cnt_df['binner_config']
                         ]
-filter_list = ['SABer_hdbscan', 'SABer_ocsvm']
+filter_list = ['SABer_denovo', 'SABer_hdbscan', 'SABer_ocsvm']
 filter_cnt_df = cat_cnt_df.query("binner not in @filter_list")
 dedup_cnt_df = filter_cnt_df.drop_duplicates(subset=['binner', 'level_mode'])
 dedup_cnt_df['binner_config_level_mode'] = [x + '_' + y for x, y
@@ -675,7 +675,7 @@ cat_cnt_df['binner'] = [x.split('_', 2)[0] + '_' + x.split('_', 2)[1]
                         if 'SABer' in x else x.split('_', 1)[0]
                         for x in cat_cnt_df['binner_config']
                         ]
-filter_list = ['SABer_hdbscan', 'SABer_ocsvm']
+filter_list = ['SABer_denovo', 'SABer_hdbscan', 'SABer_ocsvm']
 filter_cnt_df = cat_cnt_df.query("binner not in @filter_list")
 dedup_cnt_df = filter_cnt_df.drop_duplicates(subset=['binner', 'level_mode'])
 dedup_cnt_df.to_csv(os.path.join(workdir, 'tables/ALL_BINNERS.MQ.uniq_sample.counts.tsv'),

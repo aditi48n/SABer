07bd1eb68daac07e43fe6826fa4b5a6a972c18c3
Rylobyte <mclaughlinr2@gmail.com>
2022-02-10 15:40:50 -0800

    fixing size estimations and labels

--- dev_utils/saber_errstat.py
+++ dev_utils/saber_errstat.py
@@ -438,6 +438,7 @@ def runErrorAnalysis(saberout_path, synsrc_path, src_metag_file, mocksag_path, s
         cami_genome2id_dict = dict(zip(cami_genome2id_df['src_genome'],
                                        cami_genome2id_df['CAMI_genomeID']
                                        ))
+    print(cami_genome2id_dict)
     # try:
     if isfile(src2mock_file):
         src_mock_err_df = pd.read_csv(src2mock_file, sep='\t', header=0)

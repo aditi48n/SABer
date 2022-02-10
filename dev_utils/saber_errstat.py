f45d191d76a4b04c53ce02e3833f4dc9a537e1cc
Rylobyte <mclaughlinr2@gmail.com>
2022-02-10 12:49:19 -0800

    fixing mapping files

--- dev_utils/saber_errstat.py
+++ dev_utils/saber_errstat.py
@@ -622,15 +622,16 @@ def runErrorAnalysis(saberout_path, synsrc_path, src_metag_file, mocksag_path, s
                                                         'strain', 'bp_cnt']],
                                          on='contig_id', how='left'
                                          )
-    print(clust2src_df.head())
-    flurp
+
     src_bp_dict = {x: y for x, y in zip(src2contig_df['CAMI_genomeID'], src2contig_df['sum_len'])}
 
     # subset recruit dataframes
     samp_id = 'S' + str(sample_id)
+    print(src2contig_df.head())
     src2contig_df = src2contig_df.query('sample_id == @samp_id')
     contig_bp_df = contig_bp_df.query('sample_id == @samp_id')
-
+    print(src2contig_df.head())
+    flurp
     # possible bp's based on asm vs ref genome
     exact2bp_df = src2contig_df[['CAMI_genomeID', 'strain', 'sample_id', 'sum_len'
                                  ]].copy().drop_duplicates()

b943114ae531dff2e60fd778913be1b1feaa7949
Rylobyte <mclaughlinr2@gmail.com>
2022-02-11 14:58:17 -0800

    creating plasmid vs chromo

--- dev_utils/plasmid_v_chromo.py
+++ dev_utils/plasmid_v_chromo.py
@@ -1,14 +1,26 @@
+import glob
+import os
+
 import pandas as pd
 
 # specify that all columns should be shown
 pd.set_option('max_columns', None)
 
-# Files and dirs
-# denovo_out_file = glob.glob(joinpath(saberout_path, '*.denovo_clusters.tsv'))[0]
-# trusted_out_file = glob.glob(joinpath(saberout_path, '*.hdbscan_clusters.tsv'))[0]
-# ocsvm_out_file = glob.glob(joinpath(saberout_path, '*.ocsvm_clusters.tsv'))[0]
-# inter_out_file = glob.glob(joinpath(saberout_path, '*.inter_clusters.tsv'))[0]
+# Input file
 
+# Files and dirs
+saberout_path = '/home/ryan/SABer_local/benchmarking_output/' \
+                'single_binner_bench/MGE_6/SABer_single/0/' \
+                '*/*/'
+denovo_file_list = glob.glob(os.joinpath(saberout_path, '*.denovo_clusters.tsv'))
+trusted_file_list = glob.glob(os.joinpath(saberout_path, '*.hdbscan_clusters.tsv'))
+ocsvm_file_list = glob.glob(os.joinpath(saberout_path, '*.ocsvm_clusters.tsv'))
+inter_file_list = glob.glob(os.joinpath(saberout_path, '*.inter_clusters.tsv'))
+print(denovo_file_list)
+print(trusted_file_list)
+print(ocsvm_file_list)
+print(inter_file_list)
+flurp
 
 saber_single_file = '/home/ryan/SABer_local/benchmarking_output/errstat_inputs/SABer.single.errstat.tsv'
 unitem_single_file = '/home/ryan/SABer_local/benchmarking_output/errstat_inputs/UniteM.single.errstat.tsv'
@@ -23,10 +35,14 @@ saber_single_df['binner'] = ['_'.join(['SABer', str(x), str(y), str(z)])
                                  saber_single_df['param_set']
                                  )
                              ]
+saber_mge_df = saber_single_df.query("sample_type == 'MGE_6'")
 
 unitem_single_df = pd.read_csv(unitem_single_file, header=0, sep='\t')
+unitem_mge_df = unitem_single_df.query("sample_type == 'MGE_6'")
+
 vamb_multi_df = pd.read_csv(vamb_multi_file, header=0, sep='\t')
 vamb_multi_df['binner'] = 'VAMB'
+vamb_mge_df = vamb_multi_df.query("sample_type == 'MGE_6'")
 
 bin_cat_df = pd.concat([saber_single_df, unitem_single_df,
                         vamb_multi_df
